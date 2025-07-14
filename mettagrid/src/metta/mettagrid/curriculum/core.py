9be05adc1326be368a91273b6344d4d194439d67
Jack Heart <jack@loopflow.studio>
2025-07-13 20:33:29 -0700

    cp

--- mettagrid/src/metta/mettagrid/curriculum/core.py
+++ mettagrid/src/metta/mettagrid/curriculum/core.py
@@ -5,8 +5,43 @@ from omegaconf import DictConfig
 
 logger = logging.getLogger(__name__)
 
+class Task:
+    def __init__(self, id: str, curriculum: "Curriculum", env_cfg: Optional[DictConfig] = None):
+        self._id = id
+        self._is_complete = False
+        self._curricula = [(curriculum, id)]
+        self._env_cfg = env_cfg
+        self._name = self._id
+
+    def complete(self, score: float):
+        assert not self._is_complete, "Task is already complete"
+        for curriculum, id in self._curricula:
+            curriculum.complete_task(id, score)
+        self._is_complete = True
+        # logger.info(f"Task completed: {self.name()} -> {score:.5f}")
+
+    def is_complete(self):
+        return self._is_complete
+
+    def env_cfg(self) -> DictConfig:
+        assert self._env_cfg is not None, "Task has no environment configuration"
+        return self._env_cfg
+
+    def id(self) -> str:
+        return self._id
+
+    def short_name(self) -> str:
+        return self._name.split("/")[-1]
+
+class CurriculumAlgorithm:
+    
+
 
 class Curriculum:
+    def __init__(self, task_set: TaskSet, curriculum_algorithm: CurriculumAlgorithm):
+        self._task_set = task_set
+
+
     def get_task(self) -> "Task":
         raise NotImplementedError("Subclasses must implement this method")
 
@@ -56,15 +91,8 @@ class Task:
     def id(self) -> str:
         return self._id
 
-    def name(self) -> str:
-        return self._name
-
     def short_name(self) -> str:
         return self._name.split("/")[-1]
-
-    def add_parent(self, parent_curriculum: "Curriculum", parent_id: str):
-        self._curricula.append((parent_curriculum, parent_id))
-        self._name = f"{parent_id}:{self._name}"
 
 
 class SingleTaskCurriculum(Curriculum):
